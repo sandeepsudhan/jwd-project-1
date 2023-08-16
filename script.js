2e870501cbdf7a95a0147d497425e4089ebfee21
Sandeep Saravana <140026337+sandeepsudhan@users.noreply.github.com>
2023-08-15 21:57:10 -0500

    Update timeForClock function and add setInterval method

--- script.js
+++ script.js
@@ -8,7 +8,31 @@ function timeForClock() {
     var time = new Date();
     var hour = time.getHours();
     var minutes = time.getMinutes();
-    clock.innerText = hour + ':' + minutes;
+    var seconds = time.getSeconds();
+    var meridian = "AM";
+
+    if (hour > 12) {
+        hour = hour - 12;
+        meridian = "PM";
+    }
+
+    if (hour < 10) {
+        hour = "0" + hour;
+    }
+
+    if (seconds < 10) {
+        seconds = "0" + seconds;
+    }
+
+    if (minutes < 10) { // a zero will precede single-digit minutes.
+        minutes = "0" + minutes;
+    }
+
+    clock.innerText = hour + ':' + minutes + ':' + seconds + " " + meridian;
 };
 
-timeForClock();   
+timeForClock();
+
+// setInterval function executes a function (first parameter) for an interval of set milliseconds (second parameter)
+
+setInterval(timeForClock, 1000);
